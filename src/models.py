53bb4bf93276916663521ef972d4f9ee570a25f8
Ilya-Green <105509043+Ilya-Green@users.noreply.github.com>
2023-07-04 15:24:57 +0300

    alembic

--- src/models.py
+++ src/models.py
@@ -10,6 +10,17 @@ from sqlalchemy import JSON, Column, DateTime, Enum, String, Text
 
 from sqlmodel import Field, Relationship, SQLModel, MetaData
 
+NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+metadata = SQLModel.metadata
+metadata.naming_convention = NAMING_CONVENTION
+
 
 class Employee(SQLModel, table=True):
     id: Optional[int] = Field(primary_key=True)
