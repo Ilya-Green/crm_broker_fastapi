df0c16f253dda0507ac2e5eb1b93857f2b49456d
Ilya-Green <105509043+Ilya-Green@users.noreply.github.com>
2023-12-17 09:03:25 +0300

    delete notes frontend limit
    because limited on backend

--- static/js/custom_render.js
+++ static/js/custom_render.js
@@ -137,7 +137,6 @@ const render = {
     data = Array.isArray(data) ? data : [data];
     if (type != "display") return data.map((d) => d._repr).join(",");
     return `<div class="d-flex flex-row">${data
-        .slice(-1)
       .map(
         (e) =>
           `<a class='mx-1 btn-link' href="${e._detail_url}"><span class='m-1 py-1 px-2 badge bg-blue-lt lead d-inline-block text-truncate' data-toggle="tooltip" data-placement="bottom" title='${e.content}: ${e.created_at}'  style="max-width: 20em;">${e.content}: ${e.created_at}</span></a>`
