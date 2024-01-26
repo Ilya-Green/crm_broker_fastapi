24cd40a3c58cc2e56dcf5004af4c9cb4ae24d15e
Ilya-Green <105509043+Ilya-Green@users.noreply.github.com>
2024-01-26 14:21:51 +0300

    sqlalchemy engine: from sqlite to postgres params

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ from sqlalchemy import create_engine
 
 from .config import DB_URI, DB_FILE
 
-engine = create_engine(DB_URI+DB_FILE, connect_args={"check_same_thread": False}, echo=False)
+engine = create_engine(DB_URI+DB_FILE, execution_options={"batch_mode": True}, echo=False)
