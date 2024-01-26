76a70a9743f310ee8e48396bf5b3aaf74a53eb6f
Ilya-Green <105509043+Ilya-Green@users.noreply.github.com>
2024-01-26 14:22:04 +0300

    changed order of vars

--- src/config.py
+++ src/config.py
@@ -9,8 +9,8 @@ APP_TYPE = os.environ.get("APP_TYPE")
 APP_SECRET = os.environ.get("APP_SECRET")
 ADMIN_PSWD = os.environ.get("ADMIN_PSWD")
 
-DB_FILE = os.environ.get("DB_FILE")
 DB_URI = os.environ.get("DB_URI")
+DB_FILE = os.environ.get("DB_FILE")
 
 TG_TOKEN = os.environ.get("TG_TOKEN")
 TG_CHAT_ID = os.environ.get("TG_CHAT_ID")
