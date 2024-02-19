ef1540eed8bbd078bfcdac0a35da87cc24821552
Ilya-Green <105509043+Ilya-Green@users.noreply.github.com>
2024-02-19 19:30:22 +0300

    "show/hide columns" button: save state after reloading tab; columns order: save state after reloading tab

--- starlette_admin/statics/js/list.js
+++ starlette_admin/statics/js/list.js
@@ -252,6 +252,7 @@ $(function () {
 
   // Datatable instance
   var table = $("#dt").DataTable({
+    stateSave: true,
     autoWidth: false,
     dom: "r<'table-responsive't><'card-footer d-flex align-items-center'<'m-0'i><'m-0 ms-auto'p>>",
     paging: true,
